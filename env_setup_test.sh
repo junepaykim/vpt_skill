539cd59d4b767bb56285d645b6ad89cc9dc2be07
JoonPyungKim <junepay@snu.ac.kr>
2024-05-13 11:12:04 +0000

    attribute selection code

---
+++ env_setup_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+conda create -n testprompt python=3.7 -y
+source activate testprompt
+
+pip install -q tensorflow
+# specifying tfds versions is important to reproduce our results
+pip install tfds-nightly==4.4.0.dev202201080107
+pip install opencv-python
+pip install tensorflow-addons
+pip install mock
+
+
+pip install torch==1.10.1+cu102 torchvision==0.11.2+cu102 torchaudio==0.10.1 -f https://download.pytorch.org/whl/cu102/torch_stable.html
+conda install cudatoolkit=11.0
+#conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
+
+python -m pip install detectron2 -f \
+https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
+pip install opencv-python
+
+conda install tqdm pandas matplotlib seaborn scikit-learn scipy simplejson termcolor
+conda install -c iopath iopath
+
+
+# for transformers
+pip install timm==0.4.12
+pip install ml-collections
+
+# Optional: for slurm jobs
+pip install submitit -U
+pip install slurm_gpustat
