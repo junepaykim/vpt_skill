2793b4c436f0e5cd32b088e2c563edb345ec6208
JoonPyungKim <junepay@snu.ac.kr>
2024-06-02 19:26:38 +0000

    shallow tested

--- skill/generateprobe.sh
+++ skill/generateprobe.sh
@@ -3,15 +3,14 @@
 model_root=/workspace/model
 data_path=/workspace/CUB_200_2011
 output_dir=/root/nlp/vpt_skill/output
-attributes=("has_size::medium_(9_-_16_in)" "has_wing_color::yellow" "has_wing_color::red")
-target_epoch="1"
+attributes=("has_size::small_(5_-_9_in)" "has_size::very_large_(32_-_72_in)" "has_size::medium_(9_-_16_in)" "has_size::very_small_(3_-_5_in)" "has_breast_pattern::solid" "has_breast_pattern::spotted" "has_breast_pattern::striped" "has_breast_pattern::multi-colored" "has_wing_color::blue" "has_wing_color::brown" "has_wing_color::iridescent" "has_wing_color::purple" "has_wing_color::rufous" "has_wing_color::grey" "has_wing_color::yellow" "has_wing_color::olive" "has_wing_color::green" "has_wing_color::pink" "has_wing_color::orange" "has_wing_color::black" "has_wing_color::white" "has_wing_color::red" "has_wing_color::buff")
+target_epoch="80"
+lr="0.0025"
 
 for attribute in "${attributes[@]}"; do
-    for target_seed in "61" "62" "63" "64"; do
+    for target_seed in "70" "71" "72" "73" "74"; do
         python /root/nlp/vpt_skill/sk_probe.py \
             --config-file /root/nlp/vpt_skill/configs/skill/cub_binary.yaml \
-            DATA.BATCH_SIZE "32" \
-            MODEL.PROMPT.NUM_TOKENS "64" \
             SOLVER.TOTAL_EPOCH "100" \
             MODEL.PROMPT.DEEP "False" \
             MODEL.PROMPT.DROPOUT "0.1" \
@@ -22,6 +21,6 @@ for attribute in "${attributes[@]}"; do
             MODEL.MODEL_ROOT "${model_root}" \
             DATA.DATAPATH "${data_path}" \
             OUTPUT_DIR "${output_dir}/probe/${attribute}/s${target_seed}-e${target_epoch}" \
-            PROMPT_DIR "${output_dir}/${attribute}/seed${target_seed}/lr0.002/run1/prompt_ep${target_epoch}.pth"
+            PROMPT_DIR "${output_dir}/${attribute}/seed${target_seed}/lr${lr}/run1/prompt_ep${target_epoch}.pth"
     done
 done
